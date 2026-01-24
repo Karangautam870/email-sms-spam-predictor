6c3545073859b28930127787f29d002924c6cc9b
Karan Gautam <karangautam@Karans-MacBook-Air.local>
2026-01-24 19:38:25 +0530

    updated code

--- spam_classifier_streamlit.py
+++ spam_classifier_streamlit.py
@@ -4,40 +4,23 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import string
-import os
 
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
-nltk.download('punkt', quiet=True)
-nltk.download('stopwords', quiet=True)
-
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-
-tfidf_path = os.path.join(current_dir, 'vectorizer.pkl')
-model_path = os.path.join(current_dir, 'model.pkl')
-
-try:
-    with open(tfidf_path, 'rb') as f:
-        tfidf = pickle.load(f)
-    with open(model_path, 'rb') as f:
-        model = pickle.load(f)
-except FileNotFoundError as e:
-    st.error(f"Model files not found. Please ensure vectorizer.pkl and model.pkl are in the same directory as this script.")
-    st.stop()
 
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
-    
+
     y = []
     for i in text:
         if i.isalnum():
             y.append(i)
-    
+
     text = y[:]
     y.clear()
-    
+
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
@@ -45,25 +28,22 @@ def transform_text(text):
     text = y[:]
     y.clear()
     ps = PorterStemmer()
-    
+
     for i in text:
         y.append(ps.stem(i))
 
-    return " ".join(y) 
+    return " ".join(y)
+
 
 st.title('Email/SMS Spam Classifier')
 
 input_sms = st.text_area('Enter the message')
 
-if not input_sms.strip():
-    st.warning('Please enter a message to classify.')
-else:
-    if st.button('Predict'):
-        transformed_sms = tfidf.transform([transform_text(input_sms)])
+if st.button('Predict'):
+    transformed_sms = tfidf.transform([transform_text(input_sms)])
 
-        prediction = model.predict(transformed_sms)[0]
-        if prediction == 1:
-            st.error('Spam')
-        else:
-            st.success('Not Spam')
-
+    prediction = model.predict(transformed_sms)[0]
+    if prediction == 1:
+        st.error('Spam')
+    else:
+        st.success('Not Spam')
